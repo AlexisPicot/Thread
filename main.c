b977064d16b92b86af5612b24568092fc08552aa
alexis.picot <alexis.picot@cpe.fr>
2017-12-14 16:00:38 +0100

    Init

---
+++ main.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+#include <pthread.h>
+#include <zconf.h>
+#include <stdbool.h>
+
+//pthread_mutex_t mutex
+pthread_t ThreadA;
+pthread_t ThreadB;
+int compteur = 0;
+
+
+void * consommer(void * arg){
+    while (compteur<=40){
+        printf("%d",compteur);
+        fflush(stdout);
+        usleep(100000);
+        sched_yield();
+    }
+    pthread_exit(3);
+
+}
+void * produire(void *arg){
+    while (true){
+        compteur++;
+        usleep(100000);
+        sched_yield();
+    }
+}
+
+int main(int argc, char** argv) {
+
+    printf("%s",argv[1]);
+    pthread_create(&ThreadB,NULL,consommer,NULL);
+    pthread_create(&ThreadA, NULL, produire, NULL);
+    pthread_join(ThreadA,NULL);
+    pthread_cancel(ThreadB);
+}
