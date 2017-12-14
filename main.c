d0e968dd2729e1f8c7a529e1e69e7b3be11d76b0
alexis.picot <alexis.picot@cpe.fr>
2017-12-14 17:20:34 +0100

    V1

--- main.c
+++ main.c
@@ -5,39 +5,43 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutexRDVa = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutexRDVb = PTHREAD_MUTEX_INITIALIZER;
 
 pthread_t th1;
 pthread_t th2;
+pthread_t th3;
 
 
 
 void * T1(void * arg){
-    pthread_mutex_lock(&mutex);
-    puts("Je suis T1, je fais mon traitement");
+    printf("Je suis T1, j'attends le début du RDV\n");
     sleep(3);
-    puts("Je suis T1, j'ai fini");
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutexRDVa);
+    pthread_mutex_lock(&mutexRDVb);
+    puts("Le rendez vous a commencé !");
     pthread_exit(3);
 
 }
 void * T2(void * arg){
-    puts("Je suis T2, j'attends que T1 ait fini");
-    pthread_mutex_lock(&mutex);
-    puts("Je suis T2, je suis déverouillé ! Je fais mon traitement");
-    sleep(3);
-    puts("Je suis T2, j'ai fini");
+    printf("Je suis T2, j'attends le début du RDV\n");
+    sleep(5);
+    pthread_mutex_unlock(&mutexRDVb);
+    pthread_mutex_lock(&mutexRDVa);
+    puts("Le rendez vous a commencé !");
     pthread_exit(3);
+
 
 }
 
 
 int main(int argc, char** argv) {
 
-    pthread_create(&th1, NULL, T1, 0);
-    pthread_create(&th2, NULL, T2, 0);
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_lock(&mutexRDVa);
+    pthread_mutex_lock(&mutexRDVb);
 
+    pthread_create(&th1, NULL, T1, NULL);
+    pthread_create(&th2, NULL, T2, NULL);
 
     pthread_join(th1,NULL);
     pthread_join(th2,NULL);
