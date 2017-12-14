f5adecc9e1ed8866a2599fbbbbfb7dc2b33b9a23
alexis.picot <alexis.picot@cpe.fr>
2017-12-14 17:23:41 +0100

    V1

--- main.c
+++ main.c
@@ -4,20 +4,18 @@
 #include <zconf.h>
 #include <stdbool.h>
 #include <stdlib.h>
-
-pthread_mutex_t mutexRDVa = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t mutexRDVb = PTHREAD_MUTEX_INITIALIZER;
+#include "djikstra.h"
 
 pthread_t th1;
 pthread_t th2;
+pthread_t th3;
 
 
 
 void * T1(void * arg){
     printf("Je suis T1, j'attends le début du RDV\n");
     sleep(3);
-    pthread_mutex_unlock(&mutexRDVa);
-    pthread_mutex_lock(&mutexRDVb);
+    P(sem_create(1, NULL));
     puts("Le rendez vous a commencé !");
     pthread_exit(3);
 
@@ -25,8 +23,16 @@ void * T1(void * arg){
 void * T2(void * arg){
     printf("Je suis T2, j'attends le début du RDV\n");
     sleep(5);
-    pthread_mutex_unlock(&mutexRDVb);
-    pthread_mutex_lock(&mutexRDVa);
+    P(sem_create(1, NULL));
+    puts("Le rendez vous a commencé !");
+    pthread_exit(3);
+
+
+}void * T3(void * arg){
+    printf("Je suis T3, je suis le dernier arrivé, début du RDV\n");
+    sleep(5);
+    V(sem_create(1, NULL));
+    V(sem_create(1, NULL));
     puts("Le rendez vous a commencé !");
     pthread_exit(3);
 
@@ -36,14 +42,16 @@ void * T2(void * arg){
 
 int main(int argc, char** argv) {
 
-    pthread_mutex_lock(&mutexRDVa);
-    pthread_mutex_lock(&mutexRDVb);
+    sem_create(1, 0);
+
 
     pthread_create(&th1, NULL, T1, NULL);
     pthread_create(&th2, NULL, T2, NULL);
+    pthread_create(&th3, NULL, T3, NULL);
 
     pthread_join(th1,NULL);
     pthread_join(th2,NULL);
+    pthread_join(th3,NULL);
 
 
 }
