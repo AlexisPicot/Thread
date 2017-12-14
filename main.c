75aa97f74f1baa50f6975c92a5de7e1b447dbb2e
alexis.picot <alexis.picot@cpe.fr>
2017-12-14 16:23:38 +0100

    V1

--- main.c
+++ main.c
@@ -3,36 +3,34 @@
 #include <pthread.h>
 #include <zconf.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_t ThreadA;
-pthread_t ThreadB;
+pthread_t *threads;
 int compteur = 0;
 
 
-void * consommer(void * arg){
-    while (compteur<=40){
-        printf("%d",compteur);
-        fflush(stdout);
-        usleep(100000);
-        sched_yield();
-    }
+void * threadEnAttente(void * arg){
+    pthread_mutex_lock(&mutex);
+    printf("Je suis déverouillé ! Je suis le thread N°%d\n",(int)arg);
+    pthread_mutex_unlock(&mutex);
     pthread_exit(3);
 
 }
-void * produire(void *arg){
-    while (true){
-        compteur++;
-        usleep(100000);
-        sched_yield();
-    }
-}
+
 
 int main(int argc, char** argv) {
+    int n = 50;
+    pthread_mutex_lock(&mutex);
+    threads = malloc(sizeof(pthread_t)*n);
+    for (int i = 0; i < n; ++i) {
+        pthread_create(&threads[i], NULL, threadEnAttente, i);
+    }
+    pthread_mutex_unlock(&mutex);
+    for (int i = 0; i < n; ++i) {
+        pthread_join(threads[i],NULL);
+    }
 
-    printf("%s",argv[1]);
-    pthread_create(&ThreadB,NULL,consommer,NULL);
-    pthread_create(&ThreadA, NULL, produire, NULL);
-    pthread_join(ThreadA,NULL);
-    pthread_cancel(ThreadB);
+
+
 }
