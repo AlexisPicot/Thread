74a54e3dc64ff3a491ef82414bae5e4404570a19
alexis.picot <alexis.picot@cpe.fr>
2017-12-15 14:36:39 +0100

    V1

--- main.c
+++ main.c
@@ -7,9 +7,9 @@
 #include "djikstra.h"
 #define SIZE_BUFFER 3
 #define SEM_BUFFER 2
-pthread_t th1;
-pthread_t th2;
 
+pthread_mutex_t mutexBufferEcriture = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutexBufferLecture = PTHREAD_MUTEX_INITIALIZER;
 //Strucure pour le buffer, cela permet de proteger une seule variable plutot que 3
 typedef struct{
     int mem[SIZE_BUFFER];
@@ -19,7 +19,9 @@ typedef struct{
 
 
 void * Prod(void * arg){
+    puts("Je suis un producteur");
     while (true) {
+        pthread_mutex_lock(&mutexBufferEcriture);
         //Je prend un jeton, cela permet d'éviter en gros que
         // l'écrivain ait plus de SIZE_BUFFER coup d'avance
         // sur le lecteur et donc que des données soient perdues
@@ -34,12 +36,16 @@ void * Prod(void * arg){
         //Du coup on fait adresse du premier element + indice pour obtenir l'adresse en mémoire de l'élément
         buff->ptEcriture = &buff->mem[0]+(((buff->ptEcriture+1)-&buff->mem[0])%SIZE_BUFFER);
         //On attend un nombre random de secondes pour eventuellement simuler un ecrivain plus rapide que le lecteur
+        pthread_mutex_unlock(&mutexBufferEcriture);
         usleep((rand()%10)*100000);
+
     }
 
 }
 void * Conso(void * arg){
+    puts("Je suis un consommateur");
     while (true) {
+        pthread_mutex_lock(&mutexBufferLecture);
         //A l'inverse de Prod, je pose un jeton pour indiquer l'avancement
         V(sem_create(SEM_BUFFER, NULL));
         BUFFER * buff = ((BUFFER *) arg);
@@ -48,6 +54,8 @@ void * Conso(void * arg){
         //Il faut flush sinon rien ne s'affiche a la console avant la fin de l'exec
         fflush(stdout);
         buff->ptLecture = &buff->mem[0]+(((buff->ptLecture+1)-&buff->mem[0])%SIZE_BUFFER);
+
+        pthread_mutex_unlock(&mutexBufferLecture);
         usleep((rand()%10)*100000);
     }
 
@@ -55,23 +63,33 @@ void * Conso(void * arg){
 
 int main(int argc, char** argv) {
 
+    if(argc !=3)
+        perror("Il manque des arguments");
+
     srand(time(NULL));
     sem_delete(sem_create(SEM_BUFFER, NULL));
     sem_create(SEM_BUFFER, SIZE_BUFFER);
 
 
-//Initialisation du Buffer pour que les pointeurs soient sur la première case
+    //Initialisation du Buffer pour que les pointeurs soient sur la première case
     BUFFER  buff = {
             .ptEcriture=&buff.mem[0],
             .ptLecture=&buff.mem[0]
 
     };
 
-    pthread_create(&th1, NULL, Prod, (void *)&buff);
-    pthread_create(&th2, NULL, Conso, (void *)&buff);
+    for (int i = 0; i < atoi(argv[2]); ++i) {
+        pthread_t pthread;
+        pthread_create(&pthread, NULL, Prod, (void *)&buff);
+        usleep(500000);
+    }
+    for (int i = 0; i < atoi(argv[1]); ++i) {
+        pthread_t pthread;
+        pthread_create(&pthread, NULL, Conso, (void *)&buff);
+        usleep(500000);
+    }
 
-    pthread_join(th1,NULL);
-    pthread_join(th2,NULL);
+    pause();
 
 
 }
