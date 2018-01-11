9f6f14a784147964dc9b4c401c9d08fb1812e979
alexis.picot <alexis.picot@cpe.fr>
2018-01-11 16:56:54 +0100

    V1

--- main.c
+++ main.c
@@ -11,115 +11,72 @@
 #include <sys/shm.h>
 #include <sys/sem.h>
 #include <stdbool.h>
+#include <ctype.h>
 
-#define SEM_Y_FINISH        6
-
+#define SEM_FINISH        6
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
 typedef struct {
-    int y;
+    int indice;
     pthread_mutex_t mutex;
-} indice_y;
+    char const **argv;
+} mutex;
 
-indice_y  indice = {
-        .y = 0,
-        .mutex=PTHREAD_MUTEX_INITIALIZER
+mutex  indice=  {
+    .indice = 2,
+    .mutex=PTHREAD_MUTEX_INITIALIZER
 };
 
 
-
-const int size = 1000;
+int argc;
+char const ***argv;
 pthread_t *threads;
 unsigned char *image;
 
-static void calcul(int x, int y, unsigned char *pixel)
-{
-    long double p_x = (long double)x/size * 2.0l - 1.0l;
-    long double p_y = (long double)y/size * 2.0l - 1.0l;
-    long double tz = 0.7;
-    long double zoo = powl(0.5l, 13.0l * tz);
-    long double cc_x = -0.05l + p_x * zoo;
-    long double cc_y = 0.6805l + p_y * zoo;
-    long double z_x = 0.0l;
-    long double z_y = 0.0l;
-    long double m2 = 0.0l;
-    long double co = 0.0l;
-    long double dz_x = 0.0l;
-    long double dz_y = 0.0l;
-    int i;
-    for (i = 0; i < 2560; i++)
-    {
-        long double old_dz_x, old_z_x;
-        if (m2 > 1024.0l)
-            continue;
-        // Z' -> 2�Z�Z' + 1
-        old_dz_x = dz_x;
-        dz_x = 2.0l * z_x * dz_x - z_y * dz_y + 1.0l;
-        dz_y = 2.0l * z_x * dz_y + z_y * old_dz_x;
-        // Z -> Z� + c
-        old_z_x = z_x;
-        z_x = cc_x + z_x * z_x - z_y * z_y;
-        z_y = cc_y + 2.0l * old_z_x * z_y;
-        m2 = z_x * z_x + z_y * z_y;
-        co += 1.0l;
-    }
-    // distance
-    // d(c) = |Z|�log|Z|/|Z'|
-    long double d = 0.0l;
-    if (co < 2560.0l)
-    {
-        long double dot_z = z_x * z_x + z_y * z_y;
-        d = sqrtl(dot_z/(dz_x*dz_x+dz_y*dz_y)) * logl(dot_z);
-        pixel[0] = fmodl(co,256.0l);
-        d = 4.0l * d / zoo;
-        if (d < 0.0l) d = 0.0l;
-        if (d > 1.0l) d = 1.0l;
-        pixel[1] = fmodl((1.0l-d) * 255.0l * 300.0l, 255.0l);
-        d = powl(d, 50.0l*0.25l);
-        pixel[2] = d * 255.0l;
-    }
-    else
-        pixel[0]=pixel[1]=pixel[2]=0;
-}
+
 void * thread(void * arg){
-    int x,y;
+    int x;
     do {
         pthread_mutex_lock(&indice.mutex);
-        y = indice.y++;
+        x = indice.indice++;
         pthread_mutex_unlock(&indice.mutex);
+        if(x>=argc-1) break;
 
-        if(y>=size) break;
-        for (x = 0; x < size; x++) {
-            calcul(x, y, image + 3 * (y * size + x));
+        FILE* fp1, *fp2;
+        int c;
+        fp1= fopen (indice.argv[x], "r");
+        fp2= fopen (indice.argv[x], "r+");
+        if ((fp1== NULL) || (fp2== NULL)) {
+            perror ("fopen"); exit (1);
         }
+        while (c !=EOF) {
+            c=fgetc(fp1);
+            if (c!=EOF) fputc(toupper(c),fp2);
+        }
+        fclose (fp1); fclose (fp2);
+
 
     }while (1);
-        V(sem_create(SEM_Y_FINISH, NULL));
+        V(sem_create(SEM_FINISH, NULL));
 }
-int main(int argc, char const *argv[])
+int main(int _argc, char const *_argv[])
 {
-
-    FILE *file;
-    image = malloc(3*size*size);
-    int nbThread = atoi(argv[1]);
+    argc=_argc;
+    indice.argv=_argv;
+    int nbThread = MIN(atoi(_argv[1]),argc);
     pthread_mutex_unlock(&indice.mutex);
-    sem_delete(sem_create(SEM_Y_FINISH,NULL));
-    sem_create(SEM_Y_FINISH,0);
+    sem_delete(sem_create(SEM_FINISH,NULL));
+    sem_create(SEM_FINISH,0);
 
     threads = malloc(sizeof(pthread_t)*nbThread);
+
 
     for (int y = 0; y < nbThread; ++y) {
        pthread_create(&threads[y], NULL, thread,0);
     }
 
-    P(sem_create(SEM_Y_FINISH,NULL));
-
-    // �criture fichier
-    file = fopen("image.ppm", "w");
-    fprintf(file, "P6\n%d %d\n255\n", size, size);
-    fwrite(image, size, size * 3, file);
-    fclose(file);
-    free(image);
-
+    P(sem_create(SEM_FINISH,NULL));
     return 0;
 }
 
