4ac9290725be168dcf6542d5cada616ea3beeb1a
alexis.picot <alexis.picot@cpe.fr>
2018-01-12 13:55:45 +0100

    V2

--- main.c
+++ main.c
@@ -12,21 +12,20 @@
  * notre variable à protéger pour plus de clareté
  */
 typedef struct {
-    int indice;
+    int indiceFichierCourant;
     pthread_mutex_t mutex;
-    char const **argv;
+    char const **listeDesFichiers;
 } mutex;
 
-mutex  indice=  {
-        //On commence au troisième argument du tableau argv
+mutex indice = {
+        //On commence au troisième argument du tableau listeDesFichiers
         // car premier est le chemin du fichier binaire et le
         // deuxième est le nombre de thread
-    .indice = 2,
+    .indiceFichierCourant = 2,
     .mutex=PTHREAD_MUTEX_INITIALIZER
 };
 
 
-int argc;
 pthread_t *threads;
 
 
@@ -36,7 +35,7 @@ void * thread(void * arg){
         {
             //On s'apprête à modifier une variable protegée
             pthread_mutex_lock(&indice.mutex);
-            x = indice.indice++;
+            x = indice.indiceFichierCourant++;
             pthread_mutex_unlock(&indice.mutex);
         }
         //Si on a calculé toutes les lignes on sort de la boucle infinie
@@ -44,8 +43,8 @@ void * thread(void * arg){
 
         FILE* fp1, *fp2;
         int c;
-        fp1= fopen (indice.argv[x], "r");
-        fp2= fopen (indice.argv[x], "r+");
+        fp1= fopen (indice.listeDesFichiers[x], "r");
+        fp2= fopen (indice.listeDesFichiers[x], "r+");
         if ((fp1== NULL) || (fp2== NULL)) {
             perror ("fopen"); exit (1);
         }
@@ -54,11 +53,12 @@ void * thread(void * arg){
             if (c!=EOF) fputc(toupper(c),fp2);
         }
         fclose (fp1); fclose (fp2);
-
+        //On dépose un jeton quand on a fini de créer le fichier
+        V(sem_create(SEM_FINISH, NULL));
 
     }while (1);
-    //Une fois tous les fichiers traités on débloque le thread principal pour qu'il termine le programme
-        V(sem_create(SEM_FINISH, NULL));
+    //Une fois tous les fichiers traités on s'arrête
+
 }
 /**
  *
@@ -69,10 +69,10 @@ void * thread(void * arg){
  */
 int main(int _argc, char const *_argv[])
 {
-    argc=_argc;
-    indice.argv=_argv;
-    int nbThread = MIN(atoi(_argv[1]),argc);
+    indice.listeDesFichiers=_argv;
+    int nbThread = MIN(atoi(_argv[1]),_argc);
     pthread_mutex_unlock(&indice.mutex);
+    //On supprime le semaphore avant de le recréer afin d'eviter de récupérer un semaphore d'un précedent programme
     sem_delete(sem_create(SEM_FINISH,NULL));
     sem_create(SEM_FINISH,0);
 
@@ -80,10 +80,13 @@ int main(int _argc, char const *_argv[])
 
 
     for (int y = 0; y < nbThread; ++y) {
-       pthread_create(&threads[y], NULL, thread,argc);
+       pthread_create(&threads[y], NULL, thread,_argc);
     }
 
-    P(sem_create(SEM_FINISH,NULL));
+    //On prend autant de jetons qu'il y a de fichiers à traiter
+    for (int y = 2; y < _argc-1; ++y) {
+        P(sem_create(SEM_FINISH,NULL));
+    }
     return 0;
 }
 
